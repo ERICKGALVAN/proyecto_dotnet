77e0650c7554155b0745fe83aee9b1f0e99fe2dc
Erick Galvan <galvanerick27@gmail.com>
2023-08-28 07:53:45 -0600

    add migrations

---
+++ Api/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+
+
+}
