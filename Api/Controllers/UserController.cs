0c8a41da230b9a383b10aa91c162b55ddd3de422
Erick Galvan <galvanerick27@gmail.com>
2023-08-30 07:41:09 -0600

    async functions

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Api.data;
 using Api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -13,10 +14,15 @@ public class UserController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = _context.Users;
-        return Ok(users);
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
     }
 
 }
