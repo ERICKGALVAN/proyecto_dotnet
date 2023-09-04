14a13b74dea8a38296a91b43b42686d71e2daff2
Erick Galvan <galvanerick27@gmail.com>
2023-09-04 07:53:08 -0600

    fecth data

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'myApp';
+  users: any;
+
+  constructor(private http: HttpClient) {
+    console.log('AppComponent constructor called');
+  }
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/user').subscribe({
+      next: (data) => {
+        this.users = data;
+        console.log(data);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+
+      complete: () => {
+        console.log('Done');
+      },
+    });
+  }
 }
