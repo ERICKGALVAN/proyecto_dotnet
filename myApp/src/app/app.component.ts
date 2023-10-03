d88c45983732c5625d6deadaf620c988f35028e6
Erick Galvan <galvanerick27@gmail.com>
2023-10-03 08:06:33 -0600

    some changes

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { IUser } from './_models/iuser';
@@ -12,31 +11,11 @@ export class AppComponent implements OnInit {
   title = 'myApp';
   users: any;
 
-  constructor(
-    private http: HttpClient,
-    private accountService: AccountService
-  ) {
+  constructor(private accountService: AccountService) {
     console.log('AppComponent constructor called');
   }
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/user').subscribe({
-      next: (data) => {
-        this.users = data;
-        console.log(data);
-      },
-      error: (error) => {
-        console.error('There was an error!', error);
-      },
-
-      complete: () => {
-        console.log('Done');
-      },
-    });
   }
 
   setCurrentUser() {
--- myApp/src/app/home/home.component.ts
+++ myApp/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,12 +8,35 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   registerMode = false;
+  users: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUsers();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/user').subscribe({
+      next: (data) => {
+        this.users = data;
+        console.log(data);
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+
+      complete: () => {
+        console.log('Done');
+      },
+    });
+  }
+
+  cancelRegisterMode(event: boolean): void {
+    this.registerMode = event;
   }
 }
