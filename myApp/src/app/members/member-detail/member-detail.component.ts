90b601f5421aeee4a56b7b6447127ee17860c0ea
Erick Galvan <galvanerick27@gmail.com>
2023-11-13 07:28:16 -0600

    changes

--- myApp/src/app/members/member-detail/member-detail.component.ts
+++ myApp/src/app/members/member-detail/member-detail.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IMember } from 'src/app/_models/imember';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent {
+  member: IMember | undefined;
 
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(): void {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe((member) => {
+      this.member = member;
+    });
+  }
 }
